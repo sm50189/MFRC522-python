e4a7208b9f307728fa6cb505aa0426b93b8f6fc5
sm50189 <jimmy_6589@hotmail.co.th>
2017-04-15 20:20:35 +0700

    1.00

---
+++ G6reader.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
+
+class Read_RFID:
+	def get_uid(self):
+        # return uid in RFID
+        rfid_uid = []
+        # rfid_uid = [128,15,177,88]
+        MIFAREReader = MFRC522.MFRC522()
+        hex_uid = ''
+
+        while True:
+            (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+            # If a card is found
+            if status == MIFAREReader.MI_OK:
+                print "Card detected"
+            # Get the UID of the card
+            (status,uid) = MIFAREReader.MFRC522_Anticoll()
+            # If we have the UID, continue
+            if status == MIFAREReader.MI_OK:
+                rfid_uid = uid
+                break
+
+        ###################
+        # changes uid to hex number but on string
+        for i in range(len(rfid_uid)):
+            rfid_uid[i] = format(rfid_uid[i],'02x')
+
+        for i in range(len(rfid_uid)):
+            hex_uid = hex_uid + rfid_uid[i]
+
+        return hex_uid
---
+++ Test.py
@@ -0,0 +1,11 @@
+from G6reader.py import Read_RFID as RR
+
+c = r
+while c == r :
+	c = raw_input('Read(r) or End(e)')
+	if c==e:
+		break
+	else:
+		RR.get_uid
+
+
