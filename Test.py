87a52c5d2abb13f5482c37c4f0decf71e00b2c1a
sm50189 <jimmy_6589@hotmail.co.th>
2017-04-16 14:25:19 +0700

    2.6
    Move setup port to MFRC522.py

--- Test.py
+++ Test.py
@@ -2,18 +2,18 @@ from G6reader import *
 import RPi.GPIO as GPIO
 
 def main():
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(36, GPIO.OUT)
-	GPIO.setup(38, GPIO.OUT)
-	GPIO.setup(40, GPIO.OUT)
-	GPIO.setup(35, GPIO.OUT)
-	GPIO.setup(37, GPIO.OUT)
+	#GPIO.setmode(GPIO.BOARD)
+	#GPIO.setup(36, GPIO.OUT)
+	#GPIO.setup(38, GPIO.OUT)
+	#GPIO.setup(40, GPIO.OUT)
+	#GPIO.setup(35, GPIO.OUT)
+	#GPIO.setup(37, GPIO.OUT)
 	
-	GPIO.output(36, GPIO.LOW)
-	GPIO.output(38, GPIO.LOW)
-	GPIO.output(40, GPIO.LOW)
-	GPIO.output(35, GPIO.LOW)
-	GPIO.output(37, GPIO.LOW)
+	#GPIO.output(36, GPIO.LOW)
+	#GPIO.output(38, GPIO.LOW)
+	#GPIO.output(40, GPIO.LOW)
+	#GPIO.output(35, GPIO.LOW)
+	#GPIO.output(37, GPIO.LOW)
 
 	RR = Read_RFID()
 	c = 'r'
