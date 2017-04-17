07c9bd5c044874ce3927f4550739fc63c7f1c2a6
sm50189 <jimmy_6589@hotmail.co.th>
2017-04-17 20:54:52 +0700

    3.8(2)

--- Test.py
+++ Test.py
@@ -15,31 +15,42 @@ class chip_select():
 		self._port_num = port_num
 	
 	def __enter__(self):
+		print'Go LOW'
 		GPIO.output(self._port_num,GPIO.LOW)
 	
 	def __exit__(self,type,value,traceback):
+		print'Go HIGH'
 		GPIO.output(self._port_num,GPIO.HIGH)
-		time.sleep(1)
+		#time.sleep(1)
 		print "exit"
-while c == 'r' :
-	c = raw_input('Read(r) or End(e): ')
-	if c =='e':
-		RR.end_read()
-		print'End!!'
-		break
-	elif c == 'r':
-		#print'Read!!'
-		with chip_select(38) as cs:
-			print RR.get_uid()
-		#GPIO.output(24, GPIO.LOW)
-		#print RR.get_uid()
-		#GPIO.output(24, GPIO.HIGH)
-		#GPIO.output(40, GPIO.LOW)
-		with chip_select(40) as cs:
-			print RR.get_uid()
-	else:
-		c = 'r'
-		continue
+try:
+	while c == 'r' :
+		c = raw_input('Read(r) or End(e): ')
+		if c =='e':
+			RR.end_read()
+			print'End!!'
+			break
+		elif c == 'r':
+			#print'Read!!'
+			with chip_select(38) as cs:
+				print RR.get_uid()
+			with chip_select(40) as cs:
+				print RR.get_uid()
+			#GPIO.output(24, GPIO.LOW)
+			#print RR.get_uid()
+			#GPIO.output(24, GPIO.HIGH)
+			#GPIO.output(40, GPIO.LOW)
+		else:
+			c = 'r'
+			continue
+except KeyboardInterrupt:
+	RR.end_read()
+	#GPIO.cleanup()
+	print'Clean and End'
+finally:
+	RR.end_read()
+	#GPIO.cleanup()
+	print'Clean and End'
 
 
 
