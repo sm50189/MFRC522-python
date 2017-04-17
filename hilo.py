99b0989f05608784c367d2816947d940643cdbf8
sm50189 <jimmy_6589@hotmail.co.th>
2017-04-17 20:36:56 +0700

    3.5(2)
    Added hilo.py

---
+++ hilo.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+try:
+	state = 'l'
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(36, GPIO.OUT)
+	GPIO.output(36, GPIO.LOW)
+	while state == 'l' || state == 'h':
+		state = raw_input("State? LOW = 'l' HI = 'h':")
+		if state == 'l':
+			print'Go LOW'
+			GPIO.output(36, GPIO.LOW)
+		elif state == 'h':
+			print'Go HIGH'
+			GPIO.output(36, GPIO.HIGH)
+		else:
+			print'Break'
+			break
+finally:
+	GPIO.cleanup()
+	print'Clean and End'
